b8c71e0ccc3f88f7d92be368c23a68c94cda2d2d
elpatron <aidanseantucker@gmail.com>
2024-01-23 12:31:54 -0500

    too hard, failed miserably

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,6 +1,18 @@
+# Relationships: 
+# Game = can have multiple players, and many results
+# Player = can play multiple games, and many results
+# Results = each result belongs to a single player and a game
+# Game - Player is a "many to many" relationship
+
+
+
 class Game:
     def __init__(self, title):
         self.title = title
+
+    def print_title(self, title):
+        if not isinstance(title, str) and (title > 0):
+            return title
 
     def results(self):
         pass
